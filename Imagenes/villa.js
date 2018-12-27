3d5ab1eecbd1ef9b1bf6ec05f6f4c1c69f3e8f29
Roberto O <roberto.orellana.t@usach.cl>
2018-12-27 15:33:27 -0300

    dibujar vacas, pollos y cerdos

--- Imagenes/villa.js
+++ Imagenes/villa.js
@@ -1,23 +1,99 @@
+/*
+alert("yeaa!!!");
 var z;
-var vh = document.getElementById("villaHistol");
-var papel = vh.getContext("2d");
-var mapa = "tile.png";
 
-var imagen = new Image();
-imagen.src = mapa;
-imagen.addEventListener("load", dibujar);
+for (var i = 0; i < 10; i++)
+{
+    z = aleatorio(0, 10);
+    document.write(z + ", ");
+}
+*/
+
+var vk = document.getElementById("villaKof");
+var papel = vk.getContext("2d");
+
+var fondo = {
+    url: "tile.png",
+    cargaOk: false
+}
+
+var vaca = {
+    url: "vaca.png",
+    cargaOk: false
+}
+var cerdo = {
+    url: "cerdo.png",
+    cargaOk: false
+}
+var pollo = {
+    url: "pollo.png",
+    cargaOk: false
+}
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
+
+function cargarFondo()
+{
+    fondo.cargaOk = true;
+    dibujar();
+}
+
+function cargarVacas()
+{
+    vaca.cargaOk = true;
+    dibujar();
+}
+
+function cargarCerdos() {
+    cerdo.cargaOk = true;
+    dibujar();
+}
+
+function cargarPollos() {
+    pollo.cargaOk = true;
+    dibujar();
+}
 
 function dibujar()
 {
-  papel.drawImage(imagen, 0, 0);
+    if (fondo.cargaOk)
+        papel.drawImage(fondo.imagen, 0, 0);
+
+    if (vaca.cargaOk)
+        for (var v = 0; v < 10; v++)
+        {
+            var x = aleatorio(0, 420);
+            var y = aleatorio(0, 420);
+            papel.drawImage(vaca.imagen, x, y);
+        }
+
+    if (cerdo.cargaOk) {
+        var x = aleatorio(0, 420);
+        var y = aleatorio(0, 420);
+        papel.drawImage(cerdo.imagen, x, y);
+    }
+    if (pollo.cargaOk)
+        for (var v = 0; v < 10; v++)
+        {
+            var x = aleatorio(0, 420);
+            var y = aleatorio(0, 420);
+            papel.drawImage(pollo.imagen, x, y);
+        }
 }
-/*
-for (var i=0;i<10;i++)
-{
-  z= aleatorio(5,0);
-  document.whrite(z + ", ");
-}
-*/
 
 function aleatorio(min, max)
 {
